6bd279c4b4993e218fa2673fc993d387ea945072
Rakeshpotru06 <104751654+Rakeshpotru06@users.noreply.github.com>
2025-06-30 17:34:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CustomTinyMceCollab from './CustomTinymceCollab';
+import "./App.css";
 
 function App() {
   return (
